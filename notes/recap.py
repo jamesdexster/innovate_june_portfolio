f4176e9e833f4fe70e511da916a01cc7a518ab01
James Neild <jamesneild45@gmail.com>
2022-06-27 16:56:32 +0100

    added examples of various data types

--- notes/recap.py
+++ notes/recap.py
@@ -1,1 +1,13 @@
 print("This is my recap file")
+
+greeting = "Hello Everyone" # variable greeting created and defined
+
+print(greeting) # prints variable
+
+print("This is a string displaying characters")
+print("1234") # string
+print(1234+1) # interger
+print(0.01) # float
+print(True) 
+print(False) # boolean
+print(None) #none - blank/null data
